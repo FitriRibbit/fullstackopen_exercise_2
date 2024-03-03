362487a8fea02372955a6188037fd424e6dfffa4
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2024-03-04 02:16:07 +0800

    Update persons.js

--- exercise 2.6 - 2.17 (PhoneBook)/src/services/persons.js
+++ exercise 2.6 - 2.17 (PhoneBook)/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:5173/api/persons';
+const baseUrl = 'http://localhost:5173/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
